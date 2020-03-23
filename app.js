ba0502654455a4da8cabd0cf787b9a788568165f
Victor Katsuo Yamashiro <vkatsuo@gmai.com>
2020-03-23 12:37:37 -0300

    updating the variables

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const app = express();
 
 const port = 3000;
 
-const user = "email@email.com";
-const pass = "Senha1234";
+const user = "email_mudar@gmail.com";
+const pass = "senha_mudar123";
 
 app.get('/', (req, res) => res.send('Hello World'));
 
 app.get('/send', (req, res) => {
 
     const transporter = nodemailer.createTransport({
-        host: "smtp.umbler.com",
-        port: 587,
+        host: "smtp.gmail.com",
+        port: 465,
         auth: { user, pass}
     })
 
@@ -21,7 +21,7 @@ app.get('/send', (req, res) => {
     transporter.sendMail({
         from: user,
         to: user,
-        replyTo: "contato@email.com",
+        replyTo: "email_mudar@gmail.com",
         subject: "Olá, mundo!",
         html: "<h1>Hello world?</h1>",
     }).then(info=>{
